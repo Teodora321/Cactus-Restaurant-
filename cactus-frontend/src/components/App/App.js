1198f2251ee3d449dfe93c789e3f4bcf64e94d19
Teodora321 <teodoraakehaiovaa@gmail.com>
2019-12-03 11:30:21 +0200

    commit

--- cactus-frontend/src/components/App/App.js
+++ cactus-frontend/src/components/App/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import Navigation from './Navigation/Navigation';
-import Footer from './Footer/Footer';
-import Main from './Main/Main';
+import Navigation from '../Navigation/Navigation';
+import Footer from '../Footer/Footer';
+import Main from '../Main/Main';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
-import Terms from './pages-policy/Terms';
-import PrivacyPolicy from './pages-policy/Privacy-policy';
-import CookiePolicy from './pages-policy/Cookie-policy';
-import Register from './Register/Register';
-import Contacts from './Contacts/Contacts';
+import Terms from '../pages-policy/Terms';
+import PrivacyPolicy from '../pages-policy/Privacy-policy';
+import CookiePolicy from '../pages-policy/Cookie-policy';
+import Register from '../Register/Register';
+import Contacts from '../Contacts/Contacts';
 
 
 class App extends React.Component {
