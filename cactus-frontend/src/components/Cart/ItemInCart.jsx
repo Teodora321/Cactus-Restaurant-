a18cf5737b2d3a844772814e70905fd426898276
Teodora321 <teodoraakehaiovaa@gmail.com>
2019-12-12 18:54:14 +0200

    commit

--- cactus-frontend/src/components/Cart/ItemInCart.jsx
+++ cactus-frontend/src/components/Cart/ItemInCart.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { Fragment, useContext } from 'react';
 import UserContext from '../Auth/UserContext';
 
 
@@ -6,25 +6,36 @@ function ItemInCart() {
 
     const [user, setUserStatus] = useContext(UserContext);
     //imageUrl, description, title, price ?quantity;
+    console.log(user.cart)
 
     return (
-        <div className="row">
-            <div className="col-xs-2"><img className="img-responsive" src="http://placehold.it/100x70" alt="some" />
-            </div>
-            <div className="col-xs-4">
-                <h4 className="product-name"><strong>Product name</strong></h4><h4><small>Product description</small></h4>
-            </div>
-            <div className="col-xs-6">
-                <div className="col-xs-6 text-right">
-                    <h6><strong>25.00 <span className="text-muted">x</span></strong></h6>
-                </div>
-                <div className="col-xs-4">
-                    <input type="text" className="form-control input-sm" value="1" />
-                </div>
+        // <Fragment>
+    <div>
+        {
+            user.cart  &&   user.cart.map(item => {
+                return (
+                    <div className="row">
+                        <div className="col-xs-2"><img className="img-responsive" src={item.imageUrl} alt="some" />
+                        </div>
+                        <div className="col-xs-4">
+                            <h4 className="product-name"><strong>{item.title}</strong></h4><h4><small>{item.description}</small></h4>
+                        </div>
+                        <div className="col-xs-6">
+                            <div className="col-xs-6 text-right">
+                                <h6><strong>25.00 <span className="text-muted">x</span></strong></h6>
+                            </div>
+                            <div className="col-xs-4">
+                                <input type="text" className="form-control input-sm" value="1" />
+                            </div>
 
-            </div>
-        </div>
+                        </div>
+                    </div>
+                )
+            })
+        }
+
+</div>
+        {/* </Fragment> */}
     )
-
 }
 export default ItemInCart;
