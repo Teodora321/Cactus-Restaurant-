4e15e093010be8b2eca88400ac93973e6cd61ef3
Teodora321 <teodoraakehaiovaa@gmail.com>
2020-04-15 17:24:28 +0300

    adding heroku

--- back-end/config/express.js
+++ back-end/config/express.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const secret = 'secret';
+const path = require('path')
 
 module.exports = (app) => {
   app.use(cors({
@@ -12,8 +13,13 @@ module.exports = (app) => {
   app.use(bodyParser.urlencoded({
     extended: true
   }));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+  }
 
   app.use(bodyParser.json());
-
   app.use(cookieParser(secret));
 };
